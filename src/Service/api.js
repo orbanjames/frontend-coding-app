6585d243905ee05fae2b24420e5293780a1d2b5c
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-10-26 18:40:49 +0200

    subsequent commits and updates

---
+++ src/Service/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const postsUrl = 'https://dummyapi.io/data/v1/post?limit=10'
+const app_id = '6359478c5290a0a7214946e4'
+
+export const getPosts = async (id) => {
+  id = id || ''
+  try {
+    return await axios.get(`${postsUrl}${app_id}/${id}`)
+  } catch (error) {
+    console.log('Error while calling getposts api ', error)
+  }
+}
+
+export const addPost = async (post) => {
+  return await axios.post(`${postsUrl}${app_id}`, rent)
+}
+
+export const deletePost = async (id) => {
+  return await axios.delete(`${postsUrl}${app_id}/${id}`)
+}
+
+export const editPost = async (id, post) => {
+  return await axios.put(`${postsUrl}${app_id}/${id}`, post)
+}
